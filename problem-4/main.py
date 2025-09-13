9f3d56a1422071f3d2622db0759901e4dae80365
Kyle Beckstrand <beckstrandkyle@gmail.com>
2025-09-13 15:21:08 -0700

    explanation for problem 4

---
+++ problem-4/main.py
@@ -0,0 +1,8 @@
+""" 
+write function that checks if palindrome that returns True or False
+nested for loop that that starts 999 * 999 then 998...100, then 998*998 997 996...100 then 997*997 996 and so on
+each product checks for palindrome if true break and print number
+
+for the palindrome convert number into string, then reverse string and compare just using ==
+
+"""
