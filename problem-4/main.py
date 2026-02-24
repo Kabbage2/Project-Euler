dded33c9508f54db2a4515a93fcc0073991fe727
Kabbage2 <beckstrandkyle@gmail.com>
2026-02-23 21:15:01 -0800

    Added for loop

--- problem-4/main.py
+++ problem-4/main.py
@@ -1,7 +1,7 @@
 """ 
 write function that checks if palindrome that returns True or False
 nested for loop that that starts 999 * 999 then 998...100, then 998*998 997 996...100 then 997*997 996 and so on
-each product checks for palindrome if true break and print number
+each product checks for palindrome if true and compare to previous largest palindrome
 
 for the palindrome convert number into string, then reverse string and compare just using ==
 
@@ -15,4 +15,10 @@ def is_palindrome(s):
     else:
         return False
 
-    
+x = 0 
+for i in range(999, 100, -1):
+    for j in range(i, 100, -1):
+        if is_palindrome(str(i * j)) and i * j > x:
+            x = i * j
+
+print(x)
